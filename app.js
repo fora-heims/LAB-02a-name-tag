88d457e11ed862817941366dab6fcea65aabc0e6
fora-heims <foraheims@gmail.com>
2021-08-25 14:16:05 -0700

    JavaScript interactability added

---
+++ app.js
@@ -0,0 +1,28 @@
+const inputTextBox = document.getElementById('name-input');
+const updateButton = document.getElementById('update-button');
+const nameTagName = document.getElementById('name-goes-here');
+
+updateButton.addEventListener('click', () => {
+    nameTagName.textContent = inputTextBox.value;
+});
+
+const pinkButton = document.getElementById('pink-button');
+pinkButton.addEventListener('click', () => {
+    nameTagName.style.backgroundColor = 'pink';
+});
+
+const greenButton = document.getElementById('green-button');
+greenButton.addEventListener('click', () => {
+    nameTagName.style.backgroundColor = 'lightgreen';
+});
+
+const blueButton = document.getElementById('blue-button');
+blueButton.addEventListener('click', () => {
+    nameTagName.style.backgroundColor = 'lightblue';
+});
+
+const fontButton = document.getElementById('update-font-button');
+const fontDropdown = document.getElementById('font-dropdown');
+fontButton.addEventListener('click', () => {
+    nameTagName.style.fontFamily = fontDropdown.value;
+});
